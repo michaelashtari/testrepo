a28a5a6a3802c152689150752e6d16c077f46318
michaelashtari <maashtari@ucdavis.edu>
2022-05-07 21:28:58 -0700

    Create pythontest.py
    Prints nothing important and imports standard DS libraries

---
+++ pythontest.py
@@ -0,0 +1,10 @@
+# Display nothing important 
+
+print('nothing important\n')
+
+# Import frequently utilized data science libraries
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
